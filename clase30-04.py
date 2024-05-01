c69205fbf2d5e344835834169451108d8b6d28d3
kendell88 <ken.gutierrez@duocuc.cl>
2024-04-30 20:54:49 -0400

    clase 30_04

---
+++ clase30-04.py
@@ -0,0 +1,37 @@
+
+# nombre_usuario = input("Por favor, ingresa tu nombre: ")
+# print("Hola, " + nombre_usuario + "! Este es tu primer programa en Python.") 
+
+
+# print("por favor ingrese un numero ")
+# num1 = int((input))
+
+# print("por favor ingrese otro numero ")
+# num2 = int((input))
+
+# print num1>num2
+# if num1+num2:
+#     print("El " + str(num1) + "es mayor que" + str (num2))
+# else:
+#      print("El " + str(num1) + "es mayor que" + str (num2))
+
+
+# validar si un ticket esta dentro del rango valido entre  100 y 750
+
+# validar si va cancha o a galeria
+
+print("ingrese un ticket ")
+ticket=int(input())
+
+
+print ("ingrese otro ticket")
+ticket=int(input())
+
+
+if ticket >=100 and ticket <750:
+    print("el ticket esta dentro del rango valido") 
+else:
+    print ("el ticket esta fuera del rango ")
+    ticket=int (input(ingrese el numero del ticket))
+
+    verificar("ticket") 
